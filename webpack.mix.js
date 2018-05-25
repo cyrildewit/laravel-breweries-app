49b6757e04dcc732623a511b950838f1deb37162
Cyril de Wit <cyrilwit@gmail.com>
2018-05-25 22:16:25 +0200

    feat: update webpack mix configuration

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,5 +11,20 @@ let mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/assets/js/app.js', 'public/js')
-   .sass('resources/assets/sass/app.scss', 'public/css');
+// mix.js('resources/assets/js/app.js', 'public/js')
+//    .sass('resources/assets/sass/app.scss', 'public/css');
+
+// mix.copy('resources/assets/css/styles.css', 'public/css/styles.css');
+
+mix.styles([
+    'resources/assets/css/bootstrap.css',
+    'resources/assets/css/gaia.css',
+    'resources/assets/css/custom.css',
+], 'public/css/styles.css');
+
+mix.scripts([
+    'resources/assets/js/jquery.min.js',
+    'resources/assets/js/bootstrap.js',
+    'resources/assets/js/modernizr.js',
+    'resources/assets/js/gaia.js',
+], 'public/js/all.css');
